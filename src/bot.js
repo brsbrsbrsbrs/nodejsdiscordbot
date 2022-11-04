267f2a204dac48b258c2c9316bd92f58999c3a8f
BRS <90294984+brsbrsbrsbrs@users.noreply.github.com>
2022-11-04 22:19:20 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,1 +1,6 @@
+require('dotenv').config();
+const { token } process.env
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
 
+const client = Client({ intents: GatewayIntentBits.Guilds });
