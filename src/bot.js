530dc535b37732d6f761321a8874ae90dc671096
BRS <90294984+brsbrsbrsbrs@users.noreply.github.com>
2022-11-05 13:20:36 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,3 +4,13 @@ const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 
 const client = Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection()
+
+const functionFolder = fs.readdirSync(`.src/functions`);
+for (const folder of functionFolder) {
+  const functionFiles = fs
+    .readdirSync(`./src/function/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
